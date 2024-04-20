d7b6a33f7762871d0cb795c704407dd4687a4378
H. Ali <ali@Hathims-MacBook-Air.local>
2024-04-20 23:49:10 +0530

    Sum of array
    Find the sum of array of elements using pointers
    and a user-defined function.

---
+++ sum_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define n_fmt "%lli"
+typedef long long number;
+number sum (number*, size_t);
+
+int main () {
+    size_t n;
+    printf("Size: ");
+    scanf("%zu", &n);
+
+    number array[n];
+    printf("Enter %zu elements: ", n);
+    for (size_t i = 0; i < n; ++i) scanf(n_fmt, array + i);
+
+    printf("Sum: " n_fmt "\n", sum(array, n));
+    return 0;
+}
+
+number sum (number* array, size_t n) {
+   number acc = 0;
+   for (size_t i = 0; i < n; ++i) acc = acc + array[i];
+   return acc;
+}
