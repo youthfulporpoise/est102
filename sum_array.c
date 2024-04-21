f4c66f3e3eaafc09a5296d695340ddfbd3cfe430
H. Ali <ali@Hathims-MacBook-Air.local>
2024-04-21 19:30:09 +0530

    Minor changes on the last commit
    A program aim was added and minor rectifications
    were applied.

--- sum_array.c
+++ sum_array.c
@@ -1,10 +1,17 @@
+/*
+ *  THE SUM OF THE ELEMENTS OF AN ARRAY
+ *
+ *  Compute the sum of elements stored in an array using
+ *  pointers and user-defined function.
+ */
+
 #include <stdio.h>
 
 #define n_fmt "%lli"
 typedef long long number;
 number sum (number*, size_t);
 
-int main () {
+int main() {
     size_t n;
     printf("Size: ");
     scanf("%zu", &n);
@@ -17,7 +24,7 @@ int main () {
     return 0;
 }
 
-number sum (number* array, size_t n) {
+number sum(number* array, size_t n) {
    number acc = 0;
    for (size_t i = 0; i < n; ++i) acc = acc + array[i];
    return acc;
