622a3b0741f8e0208ffacdf7858fc6f92bfddc95
H. Ali <>
2024-06-14 12:23:14 +0530

    concat.c is rewritten
    A more compact input method is adopted using scanf and scansets.

--- concat.c
+++ concat.c
@@ -7,16 +7,16 @@
 #include <string.h>
 
 int main() {
-    size_t l = 64, i, j;
-    char s1[l], s2[l], s[2 * l];
+    size_t size = 64, i, j;
+    char s1[size], s2[size], s[2 * size];
 
     printf("String 1 (ending with $): ");
-    scanf("%s", s1);
+    scanf(" %63[^$]$", s1);
     printf("String 2 (also ending with $): ");
-    scanf("%s", s2);
+    scanf(" %63[^$]$", s2);
 
-    for (i = 0; s1[i] != '$' && s1[i] != '\0'; ++i) s[i] = s1[i];
-    for (j = 0; s2[j] != '$' && s2[j] != '\0'; ++j) s[i + j] = s2[j];
+    for (i = 0; s1[i] != '\0'; ++i) s[i] = s1[i];
+    for (j = 0; s2[j] != '\0'; ++j) s[i + j] = s2[j];
     s[i + j] = '\0';
 
     printf("String: %s\n", s);
